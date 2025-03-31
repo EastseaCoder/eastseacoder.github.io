aa9fcb499f576621e622b5ff887e80de3646bf62
EastseaCoder <110013811+EastseaCoder@users.noreply.github.com>
2025-03-31 16:34:45 +0900

    CSS-styling

---
+++ src/type.js
@@ -0,0 +1,11 @@
+'use strict';
+
+new TypeIt('.home__title--strong', { loop: true, speed: 100 }) //Dream Coder
+  .move(-11)
+  .type('Amazing ')
+  .pause(1000)
+  .move(null, { to: 'END' })
+  .delete()
+  .type('Front-end Engineer')
+  .pause(1000)
+  .go();
